4b21ae52842e9ae2fcf6161815410fac40c52fda
JulioKrack <julio.ruiz.dev@gmail.com>
2024-09-28 13:23:05 -0500

    siguiente clase de cors

--- routes/api.php
+++ routes/api.php
@@ -6,9 +6,16 @@ use App\Http\Controllers\ProductoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+/**
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+ */
 
 Route::apiResource('/categorias', CategoriaController::class);
 Route::apiResource('/productos', ProductoController::class);
