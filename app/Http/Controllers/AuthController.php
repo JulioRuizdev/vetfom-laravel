a41b5df268062bf87fa2dec5475fdb379959aac8
JulioKrack <julio.ruiz.dev@gmail.com>
2024-09-28 01:48:10 -0500

    configurado cors

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\RegistroRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
@@ -11,6 +12,19 @@ class AuthController extends Controller
     public function register(RegistroRequest $request)
     {
         $data = $request->validated();
+
+        $user = User::create([
+            'name' => $data['name'],
+            'phone' => $data['phone'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password']),
+        ]);
+
+        //retornar la respuesta
+        return [
+            'token' => $user->createToken('token')->plainTextToken,
+            'user' => $user,
+        ];
     }
     public function login(Request $request) {}
     public function logout(Request $request) {}
